e19ecebe8f1087d452c6b401cc2b41a91aa98e9c
Hashim Kader <hashkader07@gmail.com>
2022-12-06 15:30:20 +0200

    changed constants file

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import marketAbi from './NFTMarketplace.json';
 
-export const MarketAddress = '0x85063Ea37c7d1D1d9e6fa95202d160c83fDD6bE7';
+export const MarketAddress = '0xf5F306fe13eC77072d33C3e3448AdBF882b73473';
 export const MarketAddressABI = marketAbi.abi;
